e8f36b7e47a6c84592d159048a126983f9c660c5
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2023-07-13 17:47:01 +0300

    created base navigation for app using react-router-dom library

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-export function App() {
-  return <></>;
-}
+import { Home, Error } from "./pages";
+
+export const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  );
+};
